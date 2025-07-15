eb63ce637ff6bb7e085ca3d425f9f60c15a8cc97
Herson Guerrero Huh <83830469+herson-xyz@users.noreply.github.com>
2025-07-15 14:49:46 -0600

    Quick fixes

--- Projects/Boilerplate - R3F, TSL, WGSL/src/shaders/lenia.js
+++
@@ -1,5 +0,0 @@
-// shaders/lenia.js
-import leniaWGSL from './lenia.wgsl?raw'
-import { wgslFn } from 'three/tsl'
-
-export const leniaComputeShader = wgslFn(leniaWGSL)
--- Projects/Boilerplate - R3F, TSL, WGSL/src/shaders/smoothLife.js
+++
@@ -1,5 +0,0 @@
-// shaders/smoothLife.js
-import smoothLifeWGSL from './smoothLife.wgsl?raw'
-import { wgslFn } from 'three/tsl'
-
-export const smoothLifeComputeShader = wgslFn(smoothLifeWGSL)
