df3482bc1f4c195d65511a6311c92e00c5bb9084
Herson Guerrero Huh <83830469+herson-xyz@users.noreply.github.com>
2025-07-15 22:51:58 -0600

    Leva GUI - collapsed folders

--- Projects/Boilerplate - R3F, TSL, WGSL/src/components/SimulationParameters.js
+++ Projects/Boilerplate - R3F, TSL, WGSL/src/components/SimulationParameters.js
@@ -1,25 +1,26 @@
-import { useControls, folder} from 'leva'
+import { useControls, folder } from 'leva'
 
 export default function SimulationParameters() {
-    const params = useControls({
-      SmoothLife: folder({
-        innerRadius: { value: 1.0, min: 0.0, max: 5.0, step: 0.01 },
-        outerRadius: { value: 3.0, min: 0.0, max: 10.0, step: 0.01 },
-        B1: { value: 0.278, min: 0.0, max: 1.0, step: 0.001 },
-        B2: { value: 0.365, min: 0.0, max: 1.0, step: 0.001 },
-        D1: { value: 0.278, min: 0.0, max: 1.0, step: 0.001 },
-        D2: { value: 0.445, min: 0.0, max: 1.0, step: 0.001 },
-        M: { value: 2.0, min: 0.0, max: 10.0, step: 0.01 },
-        alpha: { value: 0.03, min: 0.0, max: 1.0, step: 0.001 },
-        beta: { value: 0.07, min: 0.0, max: 1.0, step: 0.001 },
-      }),
-      Lenia: folder({
-        R: { value: 13.0, min: 1.0, max: 30.0, step: 0.5 },
-        T: { value: 10.0, min: 1.0, max: 50.0, step: 0.1 },
-        leniaM: { value: 0.15, min: 0.0, max: 1.0, step: 0.001 },
-        S: { value: 0.015, min: 0.001, max: 0.1, step: 0.001 },
-      }),
-    })
-  
-    return params
-  }
+  const params = useControls({
+    SmoothLife: folder({
+      innerRadius: { value: 1.0, min: 0.0, max: 5.0, step: 0.01 },
+      outerRadius: { value: 3.0, min: 0.0, max: 10.0, step: 0.01 },
+      B1: { value: 0.278, min: 0.0, max: 1.0, step: 0.001 },
+      B2: { value: 0.365, min: 0.0, max: 1.0, step: 0.001 },
+      D1: { value: 0.278, min: 0.0, max: 1.0, step: 0.001 },
+      D2: { value: 0.445, min: 0.0, max: 1.0, step: 0.001 },
+      M: { value: 2.0, min: 0.0, max: 10.0, step: 0.01 },
+      alpha: { value: 0.03, min: 0.0, max: 1.0, step: 0.001 },
+      beta: { value: 0.07, min: 0.0, max: 1.0, step: 0.001 },
+    }, { collapsed: true }), // 👈 collapsed by default
+
+    Lenia: folder({
+      R: { value: 13.0, min: 1.0, max: 30.0, step: 0.5 },
+      T: { value: 10.0, min: 1.0, max: 50.0, step: 0.1 },
+      leniaM: { value: 0.15, min: 0.0, max: 1.0, step: 0.001 },
+      S: { value: 0.015, min: 0.001, max: 0.1, step: 0.001 },
+    }, { collapsed: true }) // 👈 collapsed by default
+  })
+
+  return params
+}
